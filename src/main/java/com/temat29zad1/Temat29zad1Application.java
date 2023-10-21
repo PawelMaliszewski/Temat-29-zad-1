15950972a200664512a50408742ae87ce26bcd59
Pawel Maliszewski <pablo999455@gmail.com>
2023-10-21 08:12:18 +0200

    master

---
+++ src/main/java/com/temat29zad1/Temat29zad1Application.java
@@ -0,0 +1,13 @@
+package com.temat29zad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temat29zad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Temat29zad1Application.class, args);
+    }
+
+}
